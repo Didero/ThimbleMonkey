eee714c5b3743d09dc6e6d39d103456ca1a64784
Didero <Didero@users.noreply.github.com>
2023-01-21 21:22:07 +0100

    Copy readme.md as readme.txt too, easier to open

--- pyinstaller.py
+++ pyinstaller.py
@@ -25,9 +25,12 @@ if __name__ == '__main__':
 	print("PyInstaller arguments:")
 	print(pyInstallerArguments)
 	PyInstaller.__main__.run(pyInstallerArguments)
+
 	# Copy the license and readme over too
 	for filename in ('LICENSE', 'readme.md'):
 		print(f"Copying {filename} to dist")
 		shutil.copy2(filename, 'dist')
+	print("Copying readme.md to readme.txt")
+	shutil.copy2('readme.md', os.path.join('dist', 'readme.txt'))
 
 	print(f"Build took {time.perf_counter() - startTime} seconds")
